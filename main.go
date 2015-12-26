29020e325d941caac3e83e679fcc815fbdfe78ef
pyros2097 <pyros2097@gmail.com>
2015-12-26 13:06:22 +0530

    convert spaces to tabs

--- main.go
+++ main.go
@@ -201,12 +201,12 @@ func main() {
 	}
 	if _, err = fmt.Fprint(bfd, `
 import (
-    "bytes"
-    "compress/gzip"
-    "crypto/md5"
-    "encoding/hex"
-    "io/ioutil"
-    "strings"
+	"bytes"
+	"compress/gzip"
+	"crypto/md5"
+	"encoding/hex"
+	"io/ioutil"
+	"strings"
 )
 	
 `); err != nil {
@@ -216,65 +216,65 @@ import (
 	if _, err = fmt.Fprintf(bfd, `
 // returns the contentType for the file
 func contentType(filename string) string {
-    if strings.HasSuffix(filename, ".png") {
-    	return "image/png"
-    }
-    if strings.HasSuffix(filename, ".svg") {
-    	return "image/png"
-    }
-    if strings.HasSuffix(filename, ".css") {
-    	return "text/css"
-    }
-    if strings.HasSuffix(filename, ".js") {
-      	return "application/js"
-    }
-    if strings.HasSuffix(filename, ".eot") {
-      	return "font/eot"
-    }
-    if strings.HasSuffix(filename, ".ttf") {
-      	return "font/ttf"
-    }
-    if strings.HasSuffix(filename, ".woff") || strings.HasSuffix(filename, ".woff2") {
-      	return "application/font-woff"
-    }
-    if strings.HasSuffix(filename, ".html") {
-      	return "text/html"
-    }
-    return ""
+	if strings.HasSuffix(filename, ".png") {
+		return "image/png"
+	}
+	if strings.HasSuffix(filename, ".svg") {
+		return "image/png"
+	}
+	if strings.HasSuffix(filename, ".css") {
+		return "text/css"
+	}
+	if strings.HasSuffix(filename, ".js") {
+		return "application/js"
+	}
+	if strings.HasSuffix(filename, ".eot") {
+		return "font/eot"
+	}
+	if strings.HasSuffix(filename, ".ttf") {
+		return "font/ttf"
+	}
+	if strings.HasSuffix(filename, ".woff") || strings.HasSuffix(filename, ".woff2") {
+		return "application/font-woff"
+	}
+	if strings.HasSuffix(filename, ".html") {
+		return "text/html"
+	}
+	return ""
 }
 
 // Asset Gets the file from system if debug otherwise gets it from the stored
 // data returns the data, the md5 hash of its content and its content type
 func Asset(path string, debug bool) ([]byte, string, string) {
-    if debug {
-    	var data []byte
-      	var err error
-      	var b bytes.Buffer
-      	var file string
-      	if path == "/" {
-        	file = "%s" + "index.html"
-        	data, err = ioutil.ReadFile(file)
-      	} else {
-        	file = "%s" + path
-        	data, err = ioutil.ReadFile(file)
-      	}
-      	if err != nil {
-        	file = "%s" + "index.html"
-        	data, err = ioutil.ReadFile(file)
-      	}
-      	if err != nil {
-      		return []byte("File Not Found " + file), "", "text/html"
-      	}
-      	if data != nil {
-        	w := gzip.NewWriter(&b)
-        	w.Write(data)
-        	w.Close()
-        	data = b.Bytes()
-      	}
-      	sum := md5.Sum(data)
-      	return data, hex.EncodeToString(sum[1:len(sum)]), contentType(file)
-    }
-    switch path {
+	if debug {
+		var data []byte
+		var err error
+		var b bytes.Buffer
+		var file string
+		if path == "/" {
+			file = "%s" + "index.html"
+			data, err = ioutil.ReadFile(file)
+		} else {
+			file = "%s" + path
+			data, err = ioutil.ReadFile(file)
+		}
+		if err != nil {
+			file = "%s" + "index.html"
+			data, err = ioutil.ReadFile(file)
+		}
+		if err != nil {
+			return []byte("File Not Found " + file), "", "text/html"
+		}
+		if data != nil {
+			w := gzip.NewWriter(&b)
+			w.Write(data)
+			w.Close()
+			data = b.Bytes()
+		}
+		sum := md5.Sum(data)
+		return data, hex.EncodeToString(sum[1:len(sum)]), contentType(file)
+	}
+	switch path {
 `, *input, strings.TrimSuffix(*input, "/"), *input); err != nil {
 		panic(err)
 	}
@@ -290,7 +290,7 @@ func Asset(path string, debug bool) ([]byte, string, string) {
 		}
 	}
 	if _, err = fmt.Fprintf(bfd, `	default:
-    	return %s, "%s", "text/html"
+		return %s, "%s", "text/html"
 	}
 }
 `, indexFile, randStr()); err != nil {
